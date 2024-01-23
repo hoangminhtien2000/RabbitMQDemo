e113b180a4d3f15765995e1a9b4cbe535059386f
hoangminhtien2000 <hoangminhtien2000@gmail.com>
2024-01-23 11:48:08 +0700

    nothing

--- RabbitMQ/src/main/java/com/example/rabbitmqtutorial/repository/ShipperRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.rabbitmqtutorial.repository;
-
-import com.example.rabbitmqtutorial.domain.entity.Shipper;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShipperRepo extends JpaRepository<Shipper,Integer> {
-}
